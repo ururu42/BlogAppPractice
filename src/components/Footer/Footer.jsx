92f9dbf6554caaeeb3282e3c8a2d3b84740d9f9c
ururu42 <utepusechka@gmail.com>
2025-03-31 18:33:57 +0200

    Реализовать футер

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,3 +1,49 @@
-// import styled from 'styled-components';
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
 
-export const Footer = () => <div>Футер</div>;
+const FooterContainer = ({ className }) => {
+	const [city, setCity] = useState('');
+	const [temperature, setTemperature] = useState('');
+	const [weather, setWeather] = useState('');
+
+	useEffect(() => {
+		fetch(
+			'https://api.openweathermap.org/data/2.5/weather?q=Moscow&units=metric&lang=ru&appid=ece714116b020834730da639f555e1d7',
+		)
+			.then((response) => response.json())
+			.then(({ name, main, weather }) => {
+				setCity(name);
+				setTemperature(Math.round(main.temp));
+				setWeather(weather[0].description);
+			});
+	}, []);
+	return (
+		<div className={className}>
+			<div>
+				<div>Блог веб-разработчика</div>
+				<div>web@developer.ru</div>
+			</div>
+			<div>
+				<div>
+					{city},{' '}
+					{new Date().toLocaleString('ru', { day: 'numeric', month: 'long' })}
+				</div>
+				<div>
+					{temperature} градусов, {weather}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export const Footer = styled(FooterContainer)`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	width: 1000px;
+	height: 120px;
+	padding: 20px 40px;
+	font-weight: bold;
+	background-color: #fff;
+	box-shadow: -8px 2px 15px #000;
+`;
